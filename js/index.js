c5eacfd7fdf716912c21b0b0b8dfcffb02c499c4
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-06-05 15:50:11 +0100

    added iteration 3.3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 // Iteration 1: Names and Input
 
-
 const hacker1 = "Manuel"; //Driver's name
 console.log(`The driver's name is ${hacker1}.`);
-
 
 const hacker2 = "Maria"; //Navigator's name
 console.log(`The navigator's name is ${hacker2}.`);
@@ -29,17 +27,16 @@ if (driverNameLength > navigatorNameLength) {
 
 // Iteration 3: Loops
 
-//Capitalized and space between characters for driver 
-capitalizedSpacedDriverName = '';
+//Capitalized and space between characters for driver
+capitalizedSpacedDriverName = ""; //this is a horrible name for the variable
 
 for (let i = 0; i < driverNameLength; i++) {
-  capitalizedSpacedDriverName += hacker1[i].toUpperCase() + ' ';
+  capitalizedSpacedDriverName += hacker1[i].toUpperCase() + " ";
 }
-console.log(capitalizedSpacedDriverName)
+console.log(capitalizedSpacedDriverName);
 
 //Navigator's name in reverse order
-
-reverseName = '';
+reverseName = "";
 
 lastIndex = hacker2.length - 1;
 
@@ -47,5 +44,14 @@ for (let i = lastIndex; i >= 0; i--) {
   reverseName += hacker2[i];
 }
 
-console.log(reverseName)
+console.log(reverseName);
 
+//3.3
+
+if (hacker1.localeCompare(hacker2) === -1) {
+  console.log("The drivers name goes first.");
+} else if (hacker1.localeCompare(hacker2) === 1) {
+  console.log("Yo, the navigator goes first definitely");
+} else {
+  console.log("What?! You both have the same name?");
+}
