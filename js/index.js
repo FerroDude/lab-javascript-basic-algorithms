a7ad358698f03e051d7fac0856766dc6148a4ee7
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-06-05 14:45:04 +0100

    added iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
 
+//Driver's name
+const hacker1 = 'Manuel'
+console.log(`The driver's name is ${hacker1}.`);
+
+//Navigator's name
+const hacker2 = 'Maria'
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
